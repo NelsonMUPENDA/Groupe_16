96fad177b746769243cdde8dd45f41de017e3760
Julien <zagabe.mzj@gmail.com>
2025-04-02 16:50:07 +0200

    Julien Commit Groupe 16

---
+++ TP2/TP2/Interfaces/Student.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TP2.Connection;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace TP2.Interfaces
+{
+    class Student : IPerson
+    {
+        private String name, lname,refNum;
+        public Student(String name, String lname, String refNum) 
+        {
+            this.name = name;
+            this.lname = lname;
+            this.refNum = refNum;
+        }
+
+        public String Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                this.name = value;
+            }
+        }
+        public String Lname
+        {
+            get
+            {
+                return this.lname;
+            }
+            set
+            {
+                this.lname = value;
+            }
+        }
+        public String Refnum
+        {
+            get
+            {
+                return this.refNum;
+            }
+            set
+            {
+                this.refNum = value;
+            }
+        }
+
+        public void Add()
+        {
+            dbConnection db = new dbConnection();
+            SqlConnection sqlcon = db.GetSqlConnection();
+            //MySqlConnection mysqlcon = db.GetMySqlConnection();
+
+            try
+            {
+                sqlcon.Open();
+                //mysqlcon.Open();
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    String sql = "INSERT INTO students(nom,lname,refnum)VALUES('" + name + "','" + lname + "','" + refNum + "')";
+                    SqlCommand cmd = new SqlCommand(sql, sqlcon);
+                    if (cmd.ExecuteNonQuery() == 1)
+                    {
+                        Console.WriteLine("students Inserted Successfully!!\n");
+                    }
+                    else
+                    {
+                        Console.WriteLine("students Not Inserted!!\n");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("AN ERROR WAS OCCURED WHILE TRYING TO CONNECT TO SQL SERVER");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    sqlcon.Close();
+                }
+            }
+
+        }
+
+        public void show(int id)
+        {
+            dbConnection db = new dbConnection();
+            SqlConnection sqlcon = db.GetSqlConnection();
+           // MySqlConnection mysqlcon = db.GetMySqlConnection();
+
+            try
+            {
+                sqlcon.Open();
+                //mysqlcon.Open();
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    SqlDataReader dr;
+                    String sql = "select * from students where id=" + id;
+                    SqlCommand cmd = new SqlCommand(sql, sqlcon);
+                    dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        Console.WriteLine(String.Format("Name : {0}\nLasteName : {1}\nReference Number : {2}", dr.GetString(1), dr.GetString(2), dr.GetString(3)));
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Student Not found!!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("AN ERROR WAS OCCURED WHILE TRYING TO CONNECT TO SQL SERVER");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (sqlcon.State == ConnectionState.Open)
+                {
+                    sqlcon.Close();
+                }
+            }
+        }
+    }
+}
---
+++ TP2/TP2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP2.Abstract;
+using System.Data.SqlClient;
+using TP2.Connection;
+using System.Data;
+//using MySql.Data.MySqlClient;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("ABSTRACT CLASS");
+            Console.WriteLine("===============\n");
+            Teacher t1 = new Teacher("Julien", "Mz", "0987654321");
+            Student s1 = new Student("Richard", "Muhesi", "22lsi202233");
+
+            t1.Add();
+            s1.Add();
+
+            t1.show(1);
+            Console.WriteLine("\n===============\n");
+            s1.show(1);
+
+            Console.WriteLine("\n===============\n");
+
+
+            Console.WriteLine("INTERFACE ");
+            Console.WriteLine("===============\n");
+
+            Interfaces.Teacher t2 = new Interfaces.Teacher("Bobette", "leonnie", "7263hdj");
+            Interfaces.Student s2 = new Interfaces.Student("Mupenzi", "Mata", "22lsil764523");
+
+            t2.Add();
+            s2.Add();
+
+            t2.show(2);
+            Console.WriteLine("\n===============\n");
+            s2.show(2);
+
+            Console.WriteLine("\n===============\n");
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
